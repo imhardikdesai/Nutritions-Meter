ac538512bf48739a191c61fe1f7c74a6f8c80496
ishitaraina1807 <ishitaraina18@gmail.com>
2023-10-16 19:12:11 +0530

    refactored navbar and background

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
-export default function NavBar() {
-    return (
-        <nav className="navbar navbar-expand-lg text-white navbar-dark bg-dark" >
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">NutroMeter</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/about">About</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bar">
+      <Link className="navbar-brand nav-heading" to="/">Nutrometer</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse justify-content-end nav-links" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to='/' className="nav-link" >Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/about' className="nav-link">About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
+export default NavBar;
+ 
